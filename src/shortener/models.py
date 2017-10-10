073b052ce0a2c0c722a79725d6a51d078e3976f8
lgants <lgants@gmail.com>
2017-10-09 21:28:21 -0500

    added model manager

--- src/shortener/models.py
+++ src/shortener/models.py
@@ -3,14 +3,36 @@ from .utils import code_generator, create_shortcode
 
 # Create your models here.
 
+class KirrURLManager(models.Manager):
+    def all(self, *args, **kwargs):
+        # qs is queryset
+        # qs_main is equivalent to built-in default for all
+        qs_main = super(KirrURLManager, self).all(*args, **kwargs)
+        qs = qs_main.filter(active=True)
+        return qs
+
+    def refresh_shortcodes(self):
+        qs = KirrURL.objects.filter(id__gte=1)
+        new_codes = 0
+        for q in qs:
+            q.shortcode = create_shortcode(q)
+            q.save()
+            new_codes += 1
+        return "new codes made: {i}".format(i=new_codes)
+
 class KirrURL(models.Model):
     url = models.CharField(max_length=220, )
     shortcode = models.CharField(max_length=15, unique=True, blank=True)
     timestamp = models.DateTimeField(auto_now=True) # when model last updated
     timestamp = models.DateTimeField(auto_now_add=True) # when model was created
+    active = models.BooleanField(default=True)
     # empty_dateime = models.DateTimeField(auto_now=False, auto_now_add=False)
     # shortcode = models.CharField(max_length=15, null=True) empty in db is ok
     # shortcode = models.CharField(max_length=15, default='cfedefaultshortcode')
+
+    objects = KirrURLManager()
+    some_random = KirrURLManager()
+
 
     # overwrites default save method
     def save(self, *args, **kwargs):
