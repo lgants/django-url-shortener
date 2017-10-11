3a3ebb300fbbec7afe1fdbbd6e2ccf3d04a974b9
lgants <lgants@gmail.com>
2017-10-10 19:15:29 -0500

    completed app

--- src/shortener/views.py
+++ src/shortener/views.py
@@ -20,9 +20,11 @@ def home_view_fbv(request, *args, **kwargs):
 class HomeView(View):
     def get(self, request, *args, **kwargs):
         the_form = SubmitUrlForm()
+        bg_image = 'https://upload.wikimedia.org/wikipedia/commons/0/05/20100726_Kalamitsi_Beach_Ionian_Sea_Lefkada_island_Greece.jpg'
         context = {
             "title": "Kirr.co",
-            "form": the_form
+            "form": the_form,
+            "bg_image": bg_image
         }
         return render(request, "shortener/home.html", context)
 
